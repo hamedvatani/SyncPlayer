271c3f1f294879c0de090f5acd6517760da93a41
Hamed Vatani <hamedv@gmail.com>
2024-08-17 01:41:56 +0330

    ...

---
+++ SyncPlayer/MainForm.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace SyncPlayer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.AudioPanel = new System.Windows.Forms.Panel();
+            this.StartTogetherButton = new System.Windows.Forms.Button();
+            this.EndTogetherButton = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.ArrowPanel = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(684, 386);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // AudioPanel
+            // 
+            this.AudioPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.AudioPanel.Location = new System.Drawing.Point(12, 12);
+            this.AudioPanel.Margin = new System.Windows.Forms.Padding(0);
+            this.AudioPanel.Name = "AudioPanel";
+            this.AudioPanel.Size = new System.Drawing.Size(747, 258);
+            this.AudioPanel.TabIndex = 1;
+            // 
+            // StartTogetherButton
+            // 
+            this.StartTogetherButton.Location = new System.Drawing.Point(12, 375);
+            this.StartTogetherButton.Name = "StartTogetherButton";
+            this.StartTogetherButton.Size = new System.Drawing.Size(123, 34);
+            this.StartTogetherButton.TabIndex = 2;
+            this.StartTogetherButton.Text = "Start Together";
+            this.StartTogetherButton.UseVisualStyleBackColor = true;
+            // 
+            // EndTogetherButton
+            // 
+            this.EndTogetherButton.Location = new System.Drawing.Point(141, 375);
+            this.EndTogetherButton.Name = "EndTogetherButton";
+            this.EndTogetherButton.Size = new System.Drawing.Size(123, 34);
+            this.EndTogetherButton.TabIndex = 3;
+            this.EndTogetherButton.Text = "End Together";
+            this.EndTogetherButton.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.Location = new System.Drawing.Point(603, 386);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // ArrowPanel
+            // 
+            this.ArrowPanel.Location = new System.Drawing.Point(51, 273);
+            this.ArrowPanel.Margin = new System.Windows.Forms.Padding(0);
+            this.ArrowPanel.Name = "ArrowPanel";
+            this.ArrowPanel.Size = new System.Drawing.Size(20, 20);
+            this.ArrowPanel.TabIndex = 5;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(771, 421);
+            this.Controls.Add(this.ArrowPanel);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.EndTogetherButton);
+            this.Controls.Add(this.StartTogetherButton);
+            this.Controls.Add(this.AudioPanel);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel AudioPanel;
+        private System.Windows.Forms.Button StartTogetherButton;
+        private System.Windows.Forms.Button EndTogetherButton;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel ArrowPanel;
+    }
+}
---
+++ SyncPlayer/MainForm.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
+using System.Windows.Forms;
+using NAudio.Wave;
+
+namespace SyncPlayer
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadAudioFiles(params string[] filenames)
+        {
+            var readers = new List<AudioFileReader>();
+            foreach (var filename in filenames)
+                readers.Add(new AudioFileReader(filename));
+
+
+        }
+
+        private void ShowGraphics(string[] names, TimeSpan[] lengths, TimeSpan[] silencesBefore, TimeSpan[] silencesAfter)
+        {
+            var audioHeight = 50;
+            var curveHeight = 30;
+            
+            AudioPanel.Height = names.Length * audioHeight;
+            StartTogetherButton.Top = AudioPanel.Top + AudioPanel.Height + 20;
+            EndTogetherButton.Top = AudioPanel.Top + AudioPanel.Height + 20;
+            Height = AudioPanel.Height + StartTogetherButton.Height + 80;
+            ArrowPanel.Top = AudioPanel.Top + AudioPanel.Height;
+            ArrowPanel.Left = AudioPanel.Left - 7;
+
+            var g = AudioPanel.CreateGraphics();
+            g.Clear(Color.White);
+            var maxLength = TimeSpan.Zero;
+            for (int i = 0; i < names.Length; i++)
+            {
+                var l = lengths[i] + silencesBefore[i] + silencesAfter[i];
+                if (l > maxLength)
+                    maxLength = l;
+            }
+            for (int i = 0; i < names.Length; i++)
+            {
+                var offsetY = audioHeight * i + audioHeight / 2;
+                var x1 = silencesBefore[i].TotalSeconds * AudioPanel.Width / maxLength.TotalSeconds;
+                g.DrawLine(new Pen(Color.Black),
+                    0, offsetY,
+                    (int)x1, offsetY);
+                var x2 = x1 + lengths[i].TotalSeconds * AudioPanel.Width / maxLength.TotalSeconds;
+                g.FillRectangle(new SolidBrush(Color.Blue),
+                    (int)x1, offsetY - curveHeight / 2,
+                    (int)x2 - (int)x1, curveHeight);g.DrawLine(new Pen(Color.Black),
+                    (int)x2, offsetY,
+                    AudioPanel.Width, offsetY);
+            }
+
+            g = ArrowPanel.CreateGraphics();
+            g.FillPolygon(new SolidBrush(Color.Red),
+                new Point[]
+                {
+                    new Point(8, 0),
+                    new Point(0, 13),
+                    new Point(15, 13),
+                });
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            var g = AudioPanel.CreateGraphics();
+            ShowGraphics(new string[] { "C1", "C2" },
+                new TimeSpan[] { TimeSpan.FromMinutes(4), TimeSpan.FromMinutes(5) },
+                new TimeSpan[] { TimeSpan.FromSeconds(10), TimeSpan.FromSeconds(30) },
+                new TimeSpan[] { TimeSpan.FromSeconds(15), TimeSpan.FromSeconds(10) });
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var r = new Random();
+            ArrowPanel.Left = AudioPanel.Left - 7 + r.Next(0, AudioPanel.Width);
+        }
+    }
+}
