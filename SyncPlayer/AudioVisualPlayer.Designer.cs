1b24f0d2048431e600125b589c71c3848eeed155
Vatani <H-Vatani@VATANI-PC>
2024-08-19 17:11:22 +0330

    ...

---
+++ SyncPlayer/AudioVisualPlayer.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace SyncPlayer
+{
+    partial class AudioVisualPlayer
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.WaveFormPanel = new System.Windows.Forms.Panel();
+            this.SilenceBeforeLabel = new System.Windows.Forms.Label();
+            this.AudioLabel = new System.Windows.Forms.Label();
+            this.SilenceAfterLabel = new System.Windows.Forms.Label();
+            this.TotalScrollBar = new SyncPlayer.MyScrollBar();
+            this.CurrentSilenceAfterLabel = new System.Windows.Forms.Label();
+            this.CurrentAudioLabel = new System.Windows.Forms.Label();
+            this.CurrentSilenceBeforeLabel = new System.Windows.Forms.Label();
+            this.FileNameLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // WaveFormPanel
+            // 
+            this.WaveFormPanel.Location = new System.Drawing.Point(22, 6);
+            this.WaveFormPanel.Name = "WaveFormPanel";
+            this.WaveFormPanel.Size = new System.Drawing.Size(42, 34);
+            this.WaveFormPanel.TabIndex = 0;
+            this.WaveFormPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.WaveFormPanel_Paint);
+            // 
+            // SilenceBeforeLabel
+            // 
+            this.SilenceBeforeLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.SilenceBeforeLabel.Location = new System.Drawing.Point(108, 0);
+            this.SilenceBeforeLabel.Name = "SilenceBeforeLabel";
+            this.SilenceBeforeLabel.Size = new System.Drawing.Size(50, 20);
+            this.SilenceBeforeLabel.TabIndex = 2;
+            this.SilenceBeforeLabel.Text = "00:00";
+            this.SilenceBeforeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // AudioLabel
+            // 
+            this.AudioLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.AudioLabel.Location = new System.Drawing.Point(169, 0);
+            this.AudioLabel.Name = "AudioLabel";
+            this.AudioLabel.Size = new System.Drawing.Size(50, 20);
+            this.AudioLabel.TabIndex = 3;
+            this.AudioLabel.Text = "00:00";
+            this.AudioLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // SilenceAfterLabel
+            // 
+            this.SilenceAfterLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.SilenceAfterLabel.Location = new System.Drawing.Point(230, 0);
+            this.SilenceAfterLabel.Name = "SilenceAfterLabel";
+            this.SilenceAfterLabel.Size = new System.Drawing.Size(50, 20);
+            this.SilenceAfterLabel.TabIndex = 4;
+            this.SilenceAfterLabel.Text = "00:00";
+            this.SilenceAfterLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // TotalScrollBar
+            // 
+            this.TotalScrollBar.Location = new System.Drawing.Point(108, 40);
+            this.TotalScrollBar.Name = "TotalScrollBar";
+            this.TotalScrollBar.Size = new System.Drawing.Size(177, 17);
+            this.TotalScrollBar.TabIndex = 5;
+            // 
+            // CurrentSilenceAfterLabel
+            // 
+            this.CurrentSilenceAfterLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.CurrentSilenceAfterLabel.Location = new System.Drawing.Point(230, 20);
+            this.CurrentSilenceAfterLabel.Name = "CurrentSilenceAfterLabel";
+            this.CurrentSilenceAfterLabel.Size = new System.Drawing.Size(50, 20);
+            this.CurrentSilenceAfterLabel.TabIndex = 8;
+            this.CurrentSilenceAfterLabel.Text = "00:00";
+            this.CurrentSilenceAfterLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // CurrentAudioLabel
+            // 
+            this.CurrentAudioLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.CurrentAudioLabel.Location = new System.Drawing.Point(169, 20);
+            this.CurrentAudioLabel.Name = "CurrentAudioLabel";
+            this.CurrentAudioLabel.Size = new System.Drawing.Size(50, 20);
+            this.CurrentAudioLabel.TabIndex = 7;
+            this.CurrentAudioLabel.Text = "00:00";
+            this.CurrentAudioLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // CurrentSilenceBeforeLabel
+            // 
+            this.CurrentSilenceBeforeLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.CurrentSilenceBeforeLabel.Location = new System.Drawing.Point(108, 20);
+            this.CurrentSilenceBeforeLabel.Name = "CurrentSilenceBeforeLabel";
+            this.CurrentSilenceBeforeLabel.Size = new System.Drawing.Size(50, 20);
+            this.CurrentSilenceBeforeLabel.TabIndex = 6;
+            this.CurrentSilenceBeforeLabel.Text = "00:00";
+            this.CurrentSilenceBeforeLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FileNameLabel
+            // 
+            this.FileNameLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.FileNameLabel.Location = new System.Drawing.Point(286, 0);
+            this.FileNameLabel.Name = "FileNameLabel";
+            this.FileNameLabel.Size = new System.Drawing.Size(50, 20);
+            this.FileNameLabel.TabIndex = 9;
+            this.FileNameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // AudioVisualPlayer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.FileNameLabel);
+            this.Controls.Add(this.CurrentSilenceAfterLabel);
+            this.Controls.Add(this.CurrentAudioLabel);
+            this.Controls.Add(this.CurrentSilenceBeforeLabel);
+            this.Controls.Add(this.TotalScrollBar);
+            this.Controls.Add(this.SilenceAfterLabel);
+            this.Controls.Add(this.AudioLabel);
+            this.Controls.Add(this.SilenceBeforeLabel);
+            this.Controls.Add(this.WaveFormPanel);
+            this.MinimumSize = new System.Drawing.Size(100, 80);
+            this.Name = "AudioVisualPlayer";
+            this.Size = new System.Drawing.Size(550, 80);
+            this.Load += new System.EventHandler(this.AudioVisualPlayer_Load);
+            this.Resize += new System.EventHandler(this.AudioVisualPlayer_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel WaveFormPanel;
+        private System.Windows.Forms.Label SilenceBeforeLabel;
+        private System.Windows.Forms.Label AudioLabel;
+        private System.Windows.Forms.Label SilenceAfterLabel;
+        private MyScrollBar TotalScrollBar;
+        private System.Windows.Forms.Label CurrentSilenceAfterLabel;
+        private System.Windows.Forms.Label CurrentAudioLabel;
+        private System.Windows.Forms.Label CurrentSilenceBeforeLabel;
+        private System.Windows.Forms.Label FileNameLabel;
+    }
+}
---
+++ SyncPlayer/AudioVisualPlayer.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using NAudio.Wave;
+
+namespace SyncPlayer
+{
+    public partial class AudioVisualPlayer : UserControl
+    {
+        [Category("WaveFormPanel")]
+        public bool ShowTopLine
+        {
+            get => _showTopLine;
+            set
+            {
+                if (_showTopLine != value)
+                {
+                    _showTopLine = value;
+                    WaveFormPanel_Paint(null, null);
+                }
+            }
+        }
+
+        [Category("WaveFormPanel")]
+        public bool ShowBottomLine
+        {
+            get => _showBottomLine;
+            set
+            {
+                if (_showBottomLine != value)
+                {
+                    _showBottomLine = value;
+                    WaveFormPanel_Paint(null, null);
+                }
+            }
+        }
+
+        [Category("Audio")]
+        public string Filename
+        {
+            get => _filename;
+            set
+            {
+                _filename = value;
+                if (File.Exists(_filename))
+                    _audioFileReader = new AudioFileReader(_filename);
+                AudioFileChanged();
+            }
+        }
+
+        [Category("Audio")]
+        public int SilenceBefore
+        {
+            get => _silenceBefore;
+            set
+            {
+                _silenceBefore = value;
+                AudioFileChanged();
+            }
+        }
+
+        [Category("Audio")]
+        public int SilenceAfter
+        {
+            get => _silenceAfter;
+            set
+            {
+                _silenceAfter = value;
+                AudioFileChanged();
+            }
+        }
+
+        private int AudioTime => (int)(_audioFileReader?.TotalTime.TotalSeconds ?? 0);
+
+        private bool _showTopLine;
+        private bool _showBottomLine;
+        private string _filename;
+        private int _silenceBefore;
+        private int _silenceAfter;
+        private AudioFileReader _audioFileReader;
+
+        public AudioVisualPlayer()
+        {
+            InitializeComponent();
+        }
+
+        private void AudioVisualPlayer_Load(object sender, EventArgs e)
+        {
+            AudioVisualPlayer_Resize(null, null);
+        }
+
+        private void AudioVisualPlayer_Resize(object sender, EventArgs e)
+        {
+            WaveFormPanel.Top = 0;
+            WaveFormPanel.Left = 0;
+            WaveFormPanel.Height = Height;
+            WaveFormPanel.Width = Width - 155;
+
+            FileNameLabel.Top = (Height - 80) / 2;
+            FileNameLabel.Left = Width - 150;
+            FileNameLabel.Height = 20;
+            FileNameLabel.Width = 150;
+
+            CurrentSilenceBeforeLabel.Top = 20 + (Height - 80) / 2;
+            CurrentSilenceBeforeLabel.Left = Width - 150;
+            CurrentSilenceBeforeLabel.Height = 20;
+            CurrentSilenceBeforeLabel.Width = 50;
+
+            SilenceBeforeLabel.Top = 40 + +(Height - 80) / 2;
+            SilenceBeforeLabel.Left = Width - 150;
+            SilenceBeforeLabel.Height = 20;
+            SilenceBeforeLabel.Width = 50;
+
+            CurrentAudioLabel.Top = 20 + (Height - 80) / 2;
+            CurrentAudioLabel.Left = Width - 100;
+            CurrentAudioLabel.Height = 20;
+            CurrentAudioLabel.Width = 50;
+
+            AudioLabel.Top = 40 + (Height - 80) / 2;
+            AudioLabel.Left = Width - 100;
+            AudioLabel.Height = 20;
+            AudioLabel.Width = 50;
+
+            CurrentSilenceAfterLabel.Top = 20 + (Height - 80) / 2;
+            CurrentSilenceAfterLabel.Left = Width - 50;
+            CurrentSilenceAfterLabel.Height = 20;
+            CurrentSilenceAfterLabel.Width = 50;
+
+            SilenceAfterLabel.Top = 40 + (Height - 80) / 2;
+            SilenceAfterLabel.Left = Width - 50;
+            SilenceAfterLabel.Height = 20;
+            SilenceAfterLabel.Width = 50;
+
+            TotalScrollBar.Top = 60 + (Height - 80) / 2;
+            TotalScrollBar.Left = Width - 150;
+            TotalScrollBar.Height = 17;
+            TotalScrollBar.Width = 150;
+        }
+
+        private void WaveFormPanel_Paint(object sender, PaintEventArgs e)
+        {
+            var g = WaveFormPanel.CreateGraphics();
+            g.Clear(Color.White);
+            g.DrawLine(new Pen(Color.Black)
+                , 0, 0,
+                0, WaveFormPanel.Height);
+            g.DrawLine(new Pen(Color.Black)
+                , WaveFormPanel.Width - 1, 0,
+                WaveFormPanel.Width - 1, WaveFormPanel.Height);
+            if (_showTopLine)
+                g.DrawLine(new Pen(Color.Black)
+                    , 0, 0,
+                    WaveFormPanel.Width, 0);
+            if (_showBottomLine)
+                g.DrawLine(new Pen(Color.Black
+                    ), 0, WaveFormPanel.Height - 1,
+                    WaveFormPanel.Width - 1, WaveFormPanel.Height - 1);
+
+            if (_silenceBefore < 0 ||
+                AudioTime <= 0 ||
+                _silenceAfter < 0 ||
+                _silenceBefore + AudioTime + _silenceAfter <= 0)
+                return;
+        }
+
+        private void AudioFileChanged()
+        {
+            FileNameLabel.Text = Path.GetFileName(_filename);
+            CurrentSilenceBeforeLabel.Text = ToStr(TimeSpan.Zero);
+            CurrentAudioLabel.Text = ToStr(TimeSpan.Zero);
+            CurrentSilenceAfterLabel.Text = ToStr(TimeSpan.Zero);
+            SilenceBeforeLabel.Text = ToStr(TimeSpan.FromSeconds(_silenceBefore));
+            AudioLabel.Text = ToStr(TimeSpan.FromSeconds(AudioTime));
+            SilenceAfterLabel.Text = ToStr(TimeSpan.FromSeconds(_silenceAfter));
+        }
+
+        private string ToStr(TimeSpan ts)
+        {
+            return $"{(int)ts.TotalMinutes}:{ts:ss}";
+        }
+    }
+}
